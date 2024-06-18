a8aec2e390cefec2bf4d75f6e2b22513f8a1c5ba
Utkarsh Singh <Utkarsh.Singh@Geminisolutions.com>
2024-06-18 12:08:30 +0530

    Updated Dashboard and Status

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import config as cf
 st.markdown(
     """
     <style>
-    /* Target the expander and remove padding */
+    
     .streamlit-expander {
         padding-top: 0 !important;
         padding-bottom: 0 !important;
     }
-    /* Target the notification button and hide it */
+   
     .streamlit-expander .css-15dtf11 {
         display: none !important;
     }
@@ -20,10 +20,9 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
 def check_links(links):
     status_report = []
-    for project_name, link in links:
+    for project_name, project_lead, link in links:
         if link == "n/a":
             status = "Project not deployed"
         else:
@@ -36,11 +35,11 @@ def check_links(links):
             except requests.RequestException as e:
                 status = f"Fail: {e}"
         
-        status_report.append((project_name, link, status))
+        status_report.append((project_name, project_lead, link, status))
     return status_report
 
 def generate_report(report):
-    headers = ["Project Name", "Demo Link", "Status"]
+    headers = ["Project Name", "Project Lead", "Demo Link", "Status"]
     table = tabulate(report, headers=headers, tablefmt="html")
     return table
 
@@ -48,26 +47,26 @@ def status_report_page():
     st.title("Demo Links Status Report")
     
     demo_links = [
-        ("Starzplay video enhancement POC", "https://videocrop.streamlit.app/"),
-        ("Github Bot", "https://gitbott.streamlit.app/"),
-        ("FX Sentiment Analysis", "http://3.7.234.8:8000/"),
-        ("Financial ChatBot", "https://huggingface.co/spaces/maitykritadhi/Kr_Financial_Chatbot_StreamLitUI"),
-        ("Credit Scoring model", "n/a"),
-        ("Starzplay Multi-modal Search","n/a"),
-        ("Starzplay text translation","n/a"),
-        ("Trade Surveillence POC","n/a"),
-        ("Gemini Policy Bot","n/a"),
-        ("AI virtual Influencer","n/a"),
-        ("FAB demo","http://52.66.10.81:8002/"),
-        ("Yen Forex Sentiment Analysis","http://15.206.189.243:8503/"),
-        ("Sentiment Analysis on PDF Financial Document","http://15.206.189.243:8502/"),
-        ("Airline sentiment analysis","https://usairlinessentimentanalysis-asmerbqllmx35uappbcvamo.streamlit.app"),
-        ("Emaar AI Chatbot","https://huggingface.co/spaces/anang150296/Emaar-AI-chatbot"),
-        ("Org Structure Construction","http://13.232.58.176:8003/"),
-        ("QA_GenAI","http://13.232.58.176:8004/"),
-        ("Sentiment Demo","http://13.232.58.176:8001/"),
-        ("Sentiment Analysis on Company Earnings Call Transcript","http://3.108.236.232:8503/"),
-        ("Emaar Valet AI","n/a"),
+        ("Starzplay video enhancement POC", "Aditya Singh", "https://videocrop.streamlit.app/"),
+        ("Github Bot", "Debarghya Maity", "https://gitbott.streamlit.app/"),
+        
+        ("Financial ChatBot", "Kritadhi Maity", "https://huggingface.co/spaces/maitykritadhi/Kr_Financial_Chatbot_StreamLitUI"),
+        ("Credit Scoring model", "Aditya Singh", "n/a"),
+        ("Starzplay Multi-modal Search", "Aditya Singh/Prashant Solanki", "n/a"),
+        ("Starzplay text translation", "Sushma Piraka", "n/a"),
+        ("Trade Surveillence POC", "Aditya Singh/Prashant Solanki", "n/a"),
+        ("Gemini Policy Bot", "Debarghya Maity", "n/a"),
+        ("AI virtual Influencer", "Goutam Sharma", "n/a"),
+        ("FAB demo", "Debarghya Maity", "http://52.66.10.81:8002/"),
+       
+       
+        ("Airline sentiment analysis", "Nitish John Toppo", "https://usairlinessentimentanalysis-asmerbqllmx35uappbcvamo.streamlit.app"),
+        ("Emaar IA Chatbot", "Kritadhi Maity", "https://huggingface.co/spaces/anang150296/Emaar-AI-chatbot"),
+        ("Org Structure Construction", "Debarghya Maity", "http://13.232.58.176:8003/"),
+        ("QA_GenAI", "Debarghya Maity", "http://13.232.58.176:8004/"),
+        ("Sentiment Demo", "Debarghya Maity", "http://13.232.58.176:8001/"),
+       
+        ("Emaar Valet AI", "Akshita Ranjan", "http://52.66.10.81:8001/"),
     ]
 
     if st.button("Check Links"):
@@ -77,6 +76,8 @@ def status_report_page():
             st.markdown(report_table, unsafe_allow_html=True)
     else:
         st.write("Click the button to check the status of the demo links.")
+
+
 
 def main():
 
@@ -159,7 +160,7 @@ def main():
             "deployment_links": "n/a",
             "creator": "Aditya Singh",
             "status": "n/a",
-            "code_repo_link": "n/a",
+            "code_repo_link": "https://geminisolutionsindpvtltd-my.sharepoint.com/personal/aditya_singh1_geminisolutions_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faditya%5Fsingh1%5Fgeminisolutions%5Fcom%2FDocuments%2FTrade%2DSurveillance%2DProject&ga=1",
             "description": "POC for detecting 2 (circular trading and spoofing) market manipulation techniques using synthetically generated data",
             "documentation_link": "https://docs.example.com/project2",
             "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
@@ -174,26 +175,8 @@ def main():
             "documentation_link": "https://docs.example.com/project3",
             "icon_path": os.path.join(cf.BASE_PATH, "icons", "video.jfif")
         },
-        {
-            "name": "Starzplay Video Enhancement POC",
-            "deployment_links": "https://videocrop.streamlit.app/",
-            "creator": "Chaitanya Raj",
-            "status": "Running",
-            "code_repo_link": "https://github.com/chaitanyaraj53/video_crop",
-            "description": "POC for removing in-built black borders from video and to implement 'Smart Zoom'",
-            "documentation_link": "https://docs.example.com/project4",
-            "icon_path": os.path.join(cf.BASE_PATH, "icons", "video.jfif")
-        },
-        {
-            "name": "Generic Search Test",
-            "deployment_links": "n/a",
-            "creator": "Chaitanya Raj",
-            "status": "n/a",
-            "code_repo_link": "https://colab.research.google.com/drive/1RXUMhuG_BYgW1mEoi3SdjiAIKU5oeiMI?usp=sharing",
-            "description": "Preliminary Test on Generic QA bot",
-            "documentation_link": "https://docs.example.com/project1",
-            "icon_path": os.path.join(cf.BASE_PATH, "icons", "search.png")
-        },
+        
+        
         {
             "name": "Github Bot",
             "deployment_links": "https://gitbott.streamlit.app/",
@@ -204,20 +187,11 @@ def main():
             "documentation_link": "https://docs.example.com/project1",
             "icon_path": os.path.join(cf.BASE_PATH, "icons", "chatbot.webp")
         },
-        {
-            "name": "Credit Card Transaction Anomaly",
-            "deployment_links": "n/a",
-            "creator": "Chaitanya Raj",
-            "status": "n/a",
-            "code_repo_link": "https://github.com/chaitanyaraj53/credit-card-anomaly",
-            "description": "Model training to determine fraud transactions from credit card transaction data",
-            "documentation_link": "https://docs.example.com/project1",
-            "icon_path": os.path.join(cf.BASE_PATH, "icons", "credit-card.jfif")
-        },
+        
         {
             "name": "Starzplay Multi-modal Search",
             "deployment_links": "n/a",
-            "creator": "Sushma Piraka, Saurav Anand, Aditya Singh",
+            "creator": "Saurav Anand, Aditya Singh",
             "status": "n/a",
             "code_repo_link": "https://github.com/Gemini-Solutions/multi-modal-search",
             "description": "POC when given an image as input returns the most relevant videos and when some text is given as input it will give the relevant dataframe",
@@ -227,29 +201,20 @@ def main():
         {
             "name": "Starzplay Text Translation",
             "deployment_links": "n/a",
-            "creator": "Sushma Piraka, Saurav Anand",
+            "creator": "Sushma Piraka",
             "status": "n/a",
             "code_repo_link": "https://github.com/Gemini-Solutions/text-translation",
             "description": "POC to perform multilingual translations(more specific to MENA languages)",
             "documentation_link": "https://docs.example.com/project1",
             "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
         },
-        {
-            "name": "Trade Surveillence POC",
-            "deployment_links": "n/a",
-            "creator": "Nikhil Goyal",
-            "status": "n/a",
-            "code_repo_link": "https://github.com/Gemini-Solutions/trade-surveillence",
-            "description": "POC for detecting 2 (circular trading and spoofing) market manipulation techniques using synthetically generated data",
-            "documentation_link": "https://docs.example.com/project1",
-            "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
-        },
+        
         {
             "name": "Gemini Policy Bot",
             "deployment_links": "n/a",
-            "creator": "Ankit Verma, Sushma Piraka",
+            "creator": "Debarghya Maity",
             "status": "n/a",
-            "code_repo_link": "n/a",
+            "code_repo_link": "https://github.com/prashantsolanki975/PolicyGemChat",
             "description": "POC for multi-modal chatbot which will answer employees' queries on company policies",
             "documentation_link": "https://docs.example.com/project1",
             "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
@@ -257,7 +222,7 @@ def main():
         {
             "name": "AI Virtual Influencer",
             "deployment_links": "n/a",
-            "creator": "Ashutosh Singh",
+            "creator": "Goutam Sharma",
             "status": "n/a",
             "code_repo_link": "n/a",
             "description": "It is a basic POC to display images and caption those images",
@@ -267,37 +232,18 @@ def main():
         {
             "name": "FAB Demo",
             "deployment_links": "http://52.66.10.81:8002/",
-            "creator": "Nikhil Goyal",
+            "creator": "Debarghya Maity",
             "status": "Running",
             "code_repo_link": "n/a",
             "description": "Real-time trend detection and sentiment analysis of MENA-based financial news",
             "documentation_link": "https://docs.example.com/project1",
             "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
         },
-        {
-            "name": "Yen Forex Sentiment Analysis",
-            "deployment_links": "http://15.206.189.243:8503/",
-            "creator": "Nikhil Goyal",
-            "status": "Running",
-            "code_repo_link": "https://github.com/Gemini-Solutions/yen-forex-sentiment-analysis",
-            "description": "POC to predict Forex movement based on sentiment from forex-based news articles",
-            "documentation_link": "https://docs.example.com/project1",
-            "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
-        },
-        {
-            "name": "Sentiment Analysis on PDF Financial Document",
-            "deployment_links": "http://15.206.189.243:8502/",
-            "creator": "Nikhil Goyal",
-            "status": "Running",
-            "code_repo_link": "https://github.com/Gemini-Solutions/pdf-sentiment-analysis",
-            "description": "Extracts data from financial reports in pdf format, analyzes sentiment at sentence level, and identifies important keywords",
-            "documentation_link": "https://docs.example.com/project1",
-            "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
-        },
+        
         {
             "name": "Airline Sentiment Analysis",
             "deployment_links": "https://usairlinessentimentanalysis-asmerbqllmx35uappbcvamo.streamlit.app",
-            "creator": "Nikhil Goyal",
+            "creator": "Nitish John Toppo",
             "status": "Running",
             "code_repo_link": "https://github.com/Gemini-Solutions/airline-sentiment-analysis",
             "description": "Trains model on tweets and identifies sentiments to specific US airlines",
@@ -305,9 +251,9 @@ def main():
             "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
         },
         {
-            "name": "Emaar AI Chatbot",
+            "name": "Emaar IA Chatbot",
             "deployment_links": "https://huggingface.co/spaces/anang150296/Emaar-AI-chatbot",
-            "creator": "Anang Kumar",
+            "creator": "Debarghya Maity",
             "status": "Running",
             "code_repo_link": "n/a",
             "description": "Chatbot trained on emirates policies, contact information and other relevant information",
@@ -317,7 +263,7 @@ def main():
         {
             "name": "Organizational Structure Construction",
             "deployment_links": "http://13.232.58.176:8003/",
-            "creator": "Ankit Verma",
+            "creator": "Debarghya Maity",
             "status": "Running",
             "code_repo_link": "https://github.com/Gemini-Solutions/org-structure-construction",
             "description": "Tool to construct org structure based on crawled employee information",
@@ -327,7 +273,7 @@ def main():
         {
             "name": "QA GenAI",
             "deployment_links": "http://13.232.58.176:8004/",
-            "creator": "Ankit Verma",
+            "creator": "Debarghya Maity",
             "status": "Running",
             "code_repo_link": "https://github.com/Gemini-Solutions/QA-GenAI",
             "description": "Generative AI POC for QA bot",
@@ -337,7 +283,7 @@ def main():
         {
             "name": "Sentiment Demo",
             "deployment_links": "http://13.232.58.176:8001/",
-            "creator": "Ankit Verma",
+            "creator": "Debarghya Maity",
             "status": "Running",
             "code_repo_link": "https://github.com/Gemini-Solutions/sentiment-demo",
             "description": "Demo of sentiment analysis on financial news articles",
@@ -347,7 +293,7 @@ def main():
         {
             "name": "Sentiment Analysis on Company Earnings Call Transcript",
             "deployment_links": "http://3.108.236.232:8503/",
-            "creator": "Nikhil Goyal",
+            "creator": "Debarghya Maity",
             "status": "Running",
             "code_repo_link": "https://github.com/Gemini-Solutions/earnings-call-sentiment",
             "description": "Analyzes sentiment in earnings call transcripts of various companies",
@@ -356,14 +302,25 @@ def main():
         },
         {
             "name": "Emaar Valet AI",
-            "deployment_links": "n/a",
-            "creator": "Anang Kumar",
+            "deployment_links": "http://52.66.10.81:8001/",
+            "creator": "Debarghya Maity",
             "status": "n/a",
             "code_repo_link": "n/a",
             "description": "Tool to assist Emaar employees with valet services",
             "documentation_link": "https://docs.example.com/project1",
             "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
         },
+        {
+            "name": "Financial ChatBot",
+            "deployment_links": "https://huggingface.co/spaces/maitykritadhi/Kr_Financial_Chatbot_StreamLitUI",
+            "creator": "Kritadhi Maity",
+            "status": "n/a",
+            "code_repo_link": "n/a",
+            "description": "Tool to assist Emaar employees with valet services",
+            "documentation_link": "https://docs.example.com/project1",
+            "icon_path": os.path.join(cf.BASE_PATH, "icons", "trade.png")
+        },
+       
     ]
 
     filtered_projects = [project for project in projects if search_query.lower() in project["name"].lower()]
